7483b89e164b8602d002d3dedf266f1fa922f7a3
benz <bhiseaditya7@gmail.com>
2026-02-19 14:49:26 +0530

    minor1

--- restaurant_backend/settings.py
+++ restaurant_backend/settings.py
@@ -210,6 +210,10 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ORIGIN_ALLOW_ALL = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.billfit.in",
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
